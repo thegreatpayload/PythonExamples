bc311df69d1bf5caa9ff571e08ca4e166b241186
TheGreatPayload <67967491+thegreatpayload@users.noreply.github.com>
2023-08-09 03:16:37 -0700

    Create Display Data.py

---
+++ example/Display Data.py
@@ -0,0 +1,105 @@
+#displaying Print Fuction
+a=10
+b=20
+c=a+b
+print("sum of =",c)
+print(a)
+print(b)
+print(c)
+print(a,b,c)
+
+print("#"*20)
+
+#2)
+print("Hacking With Python")
+print("#"*20)
+print("Hacking","With","Python")
+print("#"*20)
+print("Hacking"+"With"+"Python")
+print("#"*20)
+print("Hacking"+" With"+" Python")
+print("#"*20)
+s1="HWP"
+s2="is"
+s3="Comming Soon"
+print(s1,s2,s3)
+print(s1+s2+s3)
+print(s1+""+s2+""+s3)
+print("#"*20)
+sno=777
+sname="Payload"
+print("Open Port Number is"+str(sno)+"it Is A android"+sname)
+print("#"*20)
+#3)
+a=10
+b=20
+c=a+b
+print("sum =",c)
+print(c,"is the sum")
+print("#"*20)
+print("#"*20)
+a=1
+b=20
+c=30
+d=a+b+c
+print("sum of",a,",",b,"and",c,"=",d)
+print("#"*20)
+print("3)")
+print("#"*20)
+a=10
+b=20
+c=a+b
+print("sum={}".format(c))
+print("{} is the sum".format(a,b,c))
+print("Sum of {} and {}={}".format(a,b,c))
+a=10
+b=20
+c=30
+d=a+b+c
+print("sum of{},{}and{}={}".format(a,b,c,d))
+print("sum of",a,"and",b,"=",d)
+
+sno=444
+sname="Payload"
+print("Open port Number is:{} and it is a android:{}".format(sno,sname))
+print("#"*20)
+print("#"*20)
+a=10
+b=20
+c=a+b
+print("sum=%d"%c)
+print("sum of %d and %d=%d" %(a,b,c))
+print("sum(%d,%d)=%d" %(a,b,c))
+print("#"*20)
+print("#"*20)
+a=10
+b=1.2
+c=a+b
+print("sum of %d and %f=%f" %(a,b,c))
+print("Sum of %d and %0.2f=%0.3f" %(a,b,c))
+print("Sum of %0.2f and %0.2f=%0.3f" %(a,b,c))
+print("#"*20)
+print("#"*20)
+lst=[10,"HWP",19.56,True,2+3j]
+print("content of lst=%s" %str(lst))
+print("#"*20)
+d1={1:"Hacking",2:"With",3:"Python"}
+print("Content of dict=%s" %str(d1))
+print("#"*20)
+
+print("#"*20)
+
+r=range(10,21)
+for i in r:
+      print(i,end="")
+
+print("#"*20)
+
+for i in range(10,16):
+        print(i,end="\t")
+
+
+      
+
+
+
